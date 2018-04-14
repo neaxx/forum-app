9805ddfb7bc10216ab1532bb97d0da1a2d86dd2a
Aldair Duque <duquealdair@gmail.com>
2018-04-14 13:30:23 -0500

    reducer

---
+++ client/src/page/ThreadsPage/reducer.js
@@ -0,0 +1,43 @@
+const initialState = {
+
+
+	threads:[];
+	isLoading:boolean;
+	errorMessage:null;
+
+};
+
+
+
+
+export default (state = initialState, action) => {
+
+
+
+	switch (action.type) {
+
+		case 'forum-app/THREADS_LOAD_REQUEST':{
+
+			return state;
+		}
+
+
+		case 'forum-app/THREADS_LOAD_SUCCESS':{
+
+			return state;
+
+		}
+
+
+		case 'forum-app/THREADS_LOAD_FAILURE':{
+
+			return state;
+		};
+
+
+	};
+
+
+  return state;
+
+};
